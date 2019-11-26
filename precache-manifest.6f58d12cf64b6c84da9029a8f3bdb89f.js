d538373d404ee7fc3af0763ed1e0d17322f77d76
Alex Tewpin <alex.tewpin@gmail.com>
2019-11-26 21:59:12 +0300

    Updates

---
+++ precache-manifest.6f58d12cf64b6c84da9029a8f3bdb89f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24aca7e43658faee9321a6bedd0c85b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744a560b792e2ff4ea08",
+    "url": "/static/js/2.d13c56a0.chunk.js"
+  },
+  {
+    "revision": "65878175b7e7e0fbdede",
+    "url": "/static/js/main.b658ec4e.chunk.js"
+  },
+  {
+    "revision": "118e72fc61b8b5e638a9",
+    "url": "/static/js/runtime-main.1fb89f4d.js"
+  },
+  {
+    "revision": "2fef93b55ca46c20fa5c5399e458ff08",
+    "url": "/static/media/background.2fef93b5.png"
+  }
+]);
